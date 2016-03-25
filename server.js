5c4faef947490d1ffd66d4571fe403969f0ad54b
Don <don@cepave.com>
2016-03-25 13:05:43 +0800

    refine server.js

--- server.js
+++ server.js
@@ -9,8 +9,11 @@ var config = fs.readFileSync(__dirname + '/cfg.json', 'utf-8');
 var api = JSON.parse(config).api;
 var url = JSON.parse(config).url;
 
-app.get('/js/app.js', function(req, resp) {
-	var appjs = fs.readFileSync(__dirname + '/react/js/app.js', 'utf-8');
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+
+app.get('/js/app_react.js', function(req, resp) {
+	var appjs = fs.readFileSync(__dirname + '/js/app_react.js', 'utf-8');
 	resp.writeHead(200, {
 		'Content-Type': 'text/json',
 		'Access-Control-Allow-Origin': null
@@ -64,6 +67,33 @@ app.get('/readme', function(req, resp) {
 		'Access-Control-Allow-Origin': null
 	});
 	resp.write(readme);
+	resp.end();
+});
+
+app.get('/', function(req, resp) {
+	var html = fs.readFileSync(__dirname + '/react/index.html', 'utf-8');
+	resp.writeHead(200, {
+		'Content-Type': 'text/html'
+	});
+	resp.write(html);
+	resp.end();
+});
+
+app.get('/angular', function(req, resp) {
+	var html = fs.readFileSync(__dirname + '/angular/index.html', 'utf-8');
+	resp.writeHead(200, {
+		'Content-Type': 'text/html'
+	});
+	resp.write(html);
+	resp.end();
+});
+
+app.get('/react', function(req, resp) {
+	var html = fs.readFileSync(__dirname + '/react/index.html', 'utf-8');
+	resp.writeHead(200, {
+		'Content-Type': 'text/html'
+	});
+	resp.write(html);
 	resp.end();
 });
 
